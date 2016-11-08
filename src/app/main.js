eb72545df6c51a83741fa9479ef4817da18b01fa
Polad Mirzayev <polad.mirzayev@gmail.com>
2016-11-08 16:35:02 -0700

    Restructure the project

--- src/app/main.js
+++ src/app/main.js
@@ -2,8 +2,9 @@
 var dataLoader = require('./csv-loader');
 var scheduler = require('./scheduler');
 var view = require('./console-view');
+var filename = __dirname + '/../data/data.csv';
 
-dataLoader.loadFromFile('data.csv', (err, drones, locations) => {
+dataLoader.loadFromFile(filename, (err, drones, locations) => {
   if (err) { throw err; }
   view.render(
     scheduler.generate(drones, locations)
